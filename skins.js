48525a7ed45667a35284bfda77d5c3384c7ec1d6
Zeneos <95008961+Zeneos@users.noreply.github.com>
2023-07-04 17:24:57 -0400

    visual changes

--- skins.js
+++ skins.js
@@ -1,15 +1,31 @@
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search');
-const resultsList = document.getElementById('results');
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const query = document.getElementById('search-input').value;
+    fetch(`https://leagueskindatabase-b3d3c4a5ccc1.herokuapp.com/skins/search?q=${query}`)
+        .then(response => response.json())
+        .then(skins => {
+            const resultsList = document.getElementById('results-list');
+            // Clear old results
+            while (resultsList.firstChild) {
+                resultsList.firstChild.remove();
+            }
+            // Add new results
+            for (let skin of skins) {
+                const li = document.createElement('li');
+                const img = document.createElement('img');
+                img.alt = `${skin.champion} ${skin.skinName} splash art`;
+                li.textContent = `${skin.champion}: ${skin.skinName}`;
 
-searchButton.addEventListener('click', async () => {
-    const query = searchInput.value;
-    const response = await fetch(`https://leagueskindatabase-b3d3c4a5ccc1.herokuapp.com/skins/search?q=${query}`);
-    const skins = await response.json();
-    resultsList.innerHTML = '';
-    for (let skin of skins) {
-        const li = document.createElement('li');
-        li.textContent = `${skin.champion}: ${skin.skinName}`;
-        resultsList.appendChild(li);
-    }
+                // Create a new image object to preload the image
+                let tempImg = new Image();
+                tempImg.src = skin.splashUrl;
+                tempImg.onload = function () {
+                    // When the image has loaded, set the src of the img element
+                    img.src = tempImg.src;
+                }
+
+                li.appendChild(img);
+                resultsList.appendChild(li);
+            }
+        });
 });
