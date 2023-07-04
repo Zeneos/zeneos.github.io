02e46c153582d4b2cfdfee3a01971408cd76cd0a
Zeneos <95008961+Zeneos@users.noreply.github.com>
2023-07-04 14:28:50 -0400

    updates and fixes

--- skins.js
+++ skins.js
@@ -4,7 +4,7 @@ const resultsList = document.getElementById('results');
 
 searchButton.addEventListener('click', async () => {
     const query = searchInput.value;
-    const response = await fetch(`https://https://leagueskindatabase-b3d3c4a5ccc1.herokuapp.com/skins/search?q=${query}`);
+    const response = await fetch(`https://leagueskindatabase-b3d3c4a5ccc1.herokuapp.com/skins/search?q=${query}`);
     const skins = await response.json();
     resultsList.innerHTML = '';
     for (let skin of skins) {
