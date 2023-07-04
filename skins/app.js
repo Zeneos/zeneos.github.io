926b5abce494f1dac35c36985d5ce455e3fd36f7
Zeneos <95008961+Zeneos@users.noreply.github.com>
2023-07-04 14:11:50 -0400

    Update app.js

--- skins/app.js
+++ skins/app.js
@@ -3,17 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://user:teUAobt9rlRlyuMx@leagueskincluster.mwe0wxw.mongodb.net/?retryWrites=true&w=majority";
+const db = "mongodb+srv://user:teUAobt9rlRlyuMx@leagueskincluster.mwe0wxw.mongodb.net/?retryWrites=true&w=majority";
 
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Could not connect to MongoDB', err);
 });
 
 // Define schema and model
@@ -26,7 +24,7 @@ const skinSchema = new mongoose.Schema({
 
 const Skin = mongoose.model('Skin', skinSchema);
 
-// Express app
+// Create Express app
 const app = express();
 
 // Middleware
@@ -41,27 +39,12 @@ app.get('/skins', async (req, res) => {
 
 app.get('/skins/search', async (req, res) => {
     const query = req.query.q;
-    const skins = await Skin.find({ $text: {$search: query } });
+    const skins = await Skin.find({ $text: { $search: query } });
     res.json(skins);
 });
-
-// Run
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Server running on port ${port}');
+    console.log(`Server running on port ${port}`);
 });
