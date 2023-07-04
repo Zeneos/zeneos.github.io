c7edb729e0b794add4026583bdfc64c8f97d4974
Zeneos <95008961+Zeneos@users.noreply.github.com>
2023-07-04 14:04:30 -0400

    Skin database stuff

---
+++ skins.js
@@ -0,0 +1,15 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search');
+const resultsList = document.getElementById('results');
+
+searchButton.addEventListener('click', async () => {
+    const query = searchInput.value;
+    const response = await fetch(`https://https://leagueskindatabase-b3d3c4a5ccc1.herokuapp.com/skins/search?q=${query}`);
+    const skins = await response.json();
+    resultsList.innerHTML = '';
+    for (let skin of skins) {
+        const li = document.createElement('li');
+        li.textContent = `${skin.champion}: ${skin.skinName}`;
+        resultsList.appendChild(li);
+    }
+});
